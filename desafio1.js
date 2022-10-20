64ce9f2d24a85d4d9c8a9204d5341186eaabb9b9
XavierGuerrero1989 <xavierignacioguerrero@gmail.com>
2022-10-20 16:59:23 -0300

    0.1 desafio 1

--- desafio1.js
+++ desafio1.js
@@ -1,8 +1,45 @@
 class User {
-    constructor (nombre, apellido, libros, mascotas) {
+    constructor (nombre, apellido, autorLibro, nombreLibro,  mascotas) {
         this.nombre = nombre,
         this.apellido = apellido,
-        this.libros = libros,
-        this.mascotas = mascotas
+        this.libros = [{nombreLibro, autorLibro}]
+        this.mascotas = [mascotas]
+    }
+
+    getFullName() {
+        return this.nombre + " " + this.apellido
+    }
+
+    addMascota(mascotaN) {
+        this.mascotas.push(mascotaN)
+    }
+
+    countMascotas() {
+        return this.mascotas.length
+    }
+
+    addBook(nombre, autor) {
+        this.libros.push({nombreLibro: nombre, autorLibro: autor});
+    }
+
+    getBookNames() {
+        for (var i = 0; i <= (this.libros.length - 1); i++) {
+           
+            return this.libros[i].nombreLibro;
+          }
     }
 }
+
+const usuario = new User("Xavier", "Guerrero", "Stepen Hawkins", "A brief History of Time", "Matiz")
+
+
+
+usuario.addMascota("Pancho")
+usuario.addBook("TLOTR", "J.R.R. Tolkien")
+
+console.log(usuario.getFullName())
+console.log(usuario.countMascotas())
+console.log(usuario.getBookNames())
+console.log(usuario)
+
+
