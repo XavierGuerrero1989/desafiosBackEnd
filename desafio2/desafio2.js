d5debdf530eeee74702db3d81f463e83516fa6a7
XavierGuerrero1989 <xavierignacioguerrero@gmail.com>
2022-10-30 20:39:08 -0300

    desafio 2

---
+++ desafio2/desafio2.js
@@ -0,0 +1,31 @@
+class Contenedor {
+    constructor (nombreDelArchivo) {
+        this.id = id,
+        this.title = nombre,
+        this.price = precio,
+        this.thumbnail = url
+    }
+
+    saveObject(){
+
+    }
+
+    getById(){
+
+    }
+
+    getAll(){
+
+    }
+
+    deleteById(){
+
+    }
+
+    deleteAll(){
+
+    }
+
+
+
+}
