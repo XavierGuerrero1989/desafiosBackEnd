6ee23f0c37f2b19f87c946a1a32ecc20719852e5
Xavier Guerrero <xavier@alpha2000.com.ar>
2022-10-31 09:43:26 -0300

    desafio 2 1.1 terminado

--- desafio2/ejercio2.js
+++ desafio2/ejercio2.js
@@ -7,21 +7,15 @@ class Contenedor {
 
     async save (obj) {
         try {
-            // Verificamos si existe un archivo
             const archivos = await fs.promises.readFile(`./${this.name}.txt`, 'utf-8')
-            // parseamos el archivo
             const archivoParse = JSON.parse(archivos) 
-            // agregamos la funcion de marh max para obtener el id mas alto
             const ultimoId = archivoParse.reverse()
-            // pusheamos el nuevo objeto al array de archivoParse y con el id sumado + 1
             archivoParse.push({...obj, id: ultimoId[0].id + 1 })
-            // Agregamos nuevamente la lista de productos con los nuevos productos
             fs.promises.writeFile(`./${this.name}.txt`, JSON.stringify(archivoParse, null , 2))
             .then(() => console.log('Se creo exitosamente el nuevo producto con el id: ' + (ultimoId[0].id + 1) ))
             .catch(() => console.log('Hubo un error al crearse el producto'))
 
         } catch (error) {
-            // Si no existe el archivo, creamos por primera vez la lista de productos
             fs.promises.writeFile(`./${this.name}.txt`, JSON.stringify([{...obj, id:1}], null , 2))
             .then(() => console.log('Se creo exitosamente el primer producto con el id: ' + 1))
             .catch(() => console.log('Hubo un error al crearse el producto'))
@@ -32,7 +26,6 @@ class Contenedor {
     async getById (id) {
         try {
             const archivos = await fs.promises.readFile(`./${this.name}.txt`, 'utf-8')
-            // parseamos el archivo
             const archivoParse = JSON.parse(archivos)
             const productoEncontrado = archivoParse.find(archivo => archivo.id === id)
             if(!productoEncontrado) {
@@ -47,7 +40,6 @@ class Contenedor {
     async getAll () {
         try {
             const archivos = await fs.promises.readFile(`./${this.name}.txt`, 'utf-8')
-            // parseamos el archivo
             const archivoParse = JSON.parse(archivos)
             console.log(archivoParse)
         } catch (error) {
@@ -58,12 +50,10 @@ class Contenedor {
     async deleteById (id) {
         try {
             const archivos = await fs.promises.readFile(`./${this.name}.txt`, 'utf-8')
-            // parseamos el archivo
             const archivoParse = JSON.parse(archivos)
-            // Se guarda el nuevo arreglos sin el id que queremos eliminar
             const productoEncontrado = archivoParse.find(archivo => archivo.id === id)
             if(!productoEncontrado) {
-                throw new Error('No fue encontrado el producto')
+                throw new Error('El producto no Existe!')
             }
             let nuevoArray = archivoParse.filter(archivo => archivo.id !== id)
             if(!nuevoArray) {
@@ -71,7 +61,7 @@ class Contenedor {
             }
             fs.promises.writeFile(`./${this.name}.txt`, JSON.stringify(nuevoArray, null, 2))
             .then(() => console.log('El archivo fue eliminado exitosamente'))
-            .catch(() => console.log('Hubo un error al crearse el producto'))
+            .catch(() => console.log('Hubo un error al borrarse el producto'))
 
         } catch (error) {
             console.log('El archivo no existe', error)
@@ -81,9 +71,8 @@ class Contenedor {
     async deleteAll () {
         try {
             await fs.promises.readFile(`./${this.name}.txt`, 'utf-8')
-            // Se sobrescribe con un array vacio
             fs.promises.writeFile(`./${this.name}.txt`, JSON.stringify([], null, 2))
-            .then(() => console.log('Todos los archivos fueron eliminados'))
+            .then(() => console.log('Todos los productos fueron eliminados'))
             .catch(() => console.log('Hubo un error al eliminar el contenido'))
 
         } catch (error) {
