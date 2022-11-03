e3d5b5dcb8dfb682cd4351e533eec25e16d02d44
XavierGuerrero1989 <xavierignacioguerrero@gmail.com>
2022-11-02 23:06:51 -0300

    desafio 3 1.0 por terminar

--- desafio3/desafio3.js
+++ desafio3/desafio3.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-
+const express = require('express')
 
 class Contenedor {
     constructor(name){
@@ -80,13 +80,60 @@ class Contenedor {
             console.log('El archivo no existe', error)
         }
     }
+
+    // FUNCION ESPECIFICA PARA BUSCAR POR ID RANDOM, CUYO INTERVALO NO SE PASE DE LA CANTIDAD DE ELEMENTOS QUE HAY DENTRO DEL ARCHIVO
+
+    async getByIdRandom () {
+
+        try {
+            const archivos = await fs.promises.readFile(`./${this.name}.txt`, 'utf-8')
+            const archivoParse = JSON.parse(archivos)
+            let idRandom = Math.floor(Math.random() * archivoParse.length) + 1
+            const productoEncontrado = archivoParse.find(archivo => archivo.id === idRandom)
+            if(!productoEncontrado) {
+                throw new Error('No fue encontrado')
+            }
+            console.log(productoEncontrado)
+        } catch (error) {
+            console.log('El archivo no existe', error)
+        }
+    }
     
+    async getAll () {
+        try {
+            const archivos = await fs.promises.readFile(`./${this.name}.txt`, 'utf-8')
+            const archivoParse = JSON.parse(archivos)
+            console.log(archivoParse)
+        } catch (error) {
+            console.log('El archivo no existe')
+        }
+    }
 
 }
 
-const productos = new Contenedor('productos')
-productos.save({title:'titulo3', price:3, thumbnail: 'titulo3'})
+// const productos = new Contenedor('productos')
+// productos.save({title:'Escuadra', price:123.45, thumbnail: 'thumbEscuadra'})
+// productos.save({title:'Calculadora', price:234.56, thumbnail: 'thumbCalculadora'})
+// productos.save({title:'Globo Terraqueo', price:345.67, thumbnail: 'thumbGlobo'})
 // productos.getById(4)
 // productos.getAll()
 // productos.deleteById(2)
 // productos.deleteAll()
+// productos.getByIdRandom()
+
+const app = express()
+
+const PORT = 8080
+
+const server = app.listen(PORT, () => {
+    console.log('Servidor escuchando en el puerto 8080')
+})
+server.on("error", error => console.log('Error en servidor $(error)'))
+
+app.get('/productos', (req, res) => {
+    res.send({ mensaje: productos.getAll()})
+})
+
+app.get('/productoRandom', (req, res) => {
+    res.send({ mensaje: productos.getByIdRandom()})
+})
