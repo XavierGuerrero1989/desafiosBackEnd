20cf21e3420d88d25e3503136f97b2f8dbcf3735
Xavier Guerrero <xavier@alpha2000.com.ar>
2022-11-14 17:15:45 -0300

    desafio 4 aun sin terminar

---
+++ desafio4/desafio4.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const { Router } = express
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/api/productos', express.static('productos'))
+
+const productos = []
+
+const routerProductos = new Router()
+
+
+routerProductos.get('/', (req, res) => {
+    res.json(productos)
+})
+
+routerProductos.post('/', (req, res) => {
+    if (productos.length == 0){
+    productos.push({...req.body, id: 1} )
+    res.json('Se creo exitosamente el nuevo producto con el id: 1')
+    }
+
+    const ultimoId = productos.reverse()
+    productos.push({...req.body, id: ultimoId[0].id + 1} )
+    res.json('Se creo exitosamente el nuevo producto con el id: ' + (ultimoId[0].id + 1))
+})
+
+routerProductos.get('/:id', (req, res) => {
+    let id = req.param.id
+   let resultado = productos.filter( function (productos) {
+                return productos.id == id
+                
+              })
+              
+            let resultado1 = (resultado.length > 0) ? resultado[0] : null
+
+res.json(resultado1)
+})
+
+routerProductos.put('/:id', (req, res) => {
+    let id = req.params.id
+    const indexOfProd = productos.findIndex(producto => {
+        return producto.id === id;
+      });
+    productos.splice(indexOfProd, 1)
+    productos.push({...req.body, id: {id}} )
+    
+})
+
+routerProductos.delete('/:id', (req, res) => {
+    let id = req.params.id
+    const indexOfProd = productos.findIndex(producto => {
+        return producto.id === id;
+      });
+    productos.splice(indexOfProd, 1)
+})
+
+
+app.use('/productos', routerProductos)
+
+  
+const server = app.listen(8080, () => {
+    console.log('escuchando en el 8080')
+})
+
+
