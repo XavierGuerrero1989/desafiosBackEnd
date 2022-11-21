2436b04b79d1bd18d7c7a27d9dd62ea84fc68a79
XavierGuerrero1989 <xavierignacioguerrero@gmail.com>
2022-11-21 09:22:57 -0300

    desafio 5 pug a medias

--- desafio5/Pug/server.js
+++ desafio5/Pug/server.js
@@ -7,8 +7,8 @@ const app = express()
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/datos', (req, res) => {
-    res.render('nivel', req.query)
+app.get('/', (req, res) => {
+    res.render('formulario', req.query)
 })
 
 app.listen(8080)
